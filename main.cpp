c976bb669751278d1930afc954a493ca07a0800c
Duenkova Ulyana <ulana.duenkova@gmail.com>
2024-12-16 22:07:24 +0300

    Добавление умножения

--- main.cpp
+++ main.cpp
@@ -9,5 +9,6 @@ int main(){
 	std::cin >> a >> b;
 	std::cout << "a + b =" << a+b << std::endl;
 	std::cout << "a - b =" << a-b << std::endl;
+	std::cout << "a * b =" << a*b << std::endl;
 	return 0;
 }
